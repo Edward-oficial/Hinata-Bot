c09557e41327f1b2c26cec69d6188d5eba5e4b90
El vigilante <cololacalempira5@gmail.com>
2026-07-03 09:15:04 -0600

    Actualizar group-open.js

--- plugins/group-open.js
+++ plugins/group-open.js
@@ -1,10 +1,60 @@
 let handler = async (m, { conn, isAdmin, isBotAdmin }) => {
-  if (!m.isGroup) return conn.sendMessage(m.chat, { text: '👥 「 HINATA OPEN 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo para grupos\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA OPEN 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ Solo administradores\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
-  if (!isBotAdmin) return conn.sendMessage(m.chat, { text: '👥 「 HINATA OPEN 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n❥ La bot necesita ser admin\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  if (!m.isGroup) return conn.sendMessage(m.chat, { 
+    text: '*_Hinata-Bot_*\n\n➮ *_SOLO GRUPOS_*\n✰ Este comando solo funciona en grupos',
+    contextInfo: {
+      mentionedJid: [m.sender],
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363407253203904@newsletter',
+        newsletterName: '𓆩⚝𓆪 ʜɪɴᴀᴛᴀ ᴏꜰɪᴄɪᴀʟ 𓆩⚝𓆪',
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
+  
+  if (!isAdmin) return conn.sendMessage(m.chat, { 
+    text: '*_Hinata-Bot_*\n\n➮ *_SOLO ADMINS_*\n✰ Solo administradores pueden usar este comando',
+    contextInfo: {
+      mentionedJid: [m.sender],
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363407253203904@newsletter',
+        newsletterName: '𓆩⚝𓆪 ʜɪɴᴀᴛᴀ ᴏꜰɪᴄɪᴀʟ 𓆩⚝𓆪',
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
+  
+  if (!isBotAdmin) return conn.sendMessage(m.chat, { 
+    text: '*_Hinata-Bot_*\n\n➮ *_NECESITO SER ADMIN_*\n✰ La bot necesita ser administradora',
+    contextInfo: {
+      mentionedJid: [m.sender],
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363407253203904@newsletter',
+        newsletterName: '𓆩⚝𓆪 ʜɪɴᴀᴛᴀ ᴏꜰɪᴄɪᴀʟ 𓆩⚝𓆪',
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
 
   await conn.groupSettingUpdate(m.chat, 'not_announcement')
-  await conn.sendMessage(m.chat, { text: '👥 「 HINATA OPEN 」 👥\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔\n\n🔓 » Grupo abierto\n💬 » Todos pueden hablar\n\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔' }, { quoted: m })
+  await conn.sendMessage(m.chat, { 
+    text: '*_Hinata-Bot_*\n\n➮ *_GRUPO ABIERTO_*\n✰ Grupo abierto exitosamente\n✰ Todos los miembros pueden enviar mensajes',
+    contextInfo: {
+      mentionedJid: [m.sender],
+      forwardingScore: 999,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363407253203904@newsletter',
+        newsletterName: '𓆩⚝𓆪 ʜɪɴᴀᴛᴀ ᴏꜰɪᴄɪᴀʟ 𓆩⚝𓆪',
+        serverMessageId: 1
+      }
+    }
+  }, { quoted: m })
 }
 
 handler.help = ['open']
